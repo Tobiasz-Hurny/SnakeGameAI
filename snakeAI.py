55e7b494a0b0b294bc7c2e2349a1ee5ff9960b7c
Tobi <75902541+Tobiasz-Hurny@users.noreply.github.com>
2023-04-11 19:32:13 +0200

    Update

--- snakeAI.py
+++ snakeAI.py
@@ -20,9 +20,9 @@ Point = namedtuple('Point', 'x , y')
 BLOCK_SIZE = 20
 SPEED = 40
 WHITE = (255, 255, 255)
-RED = (200, 0, 0)
-BLUE1 = (0, 0, 255)
-BLUE2 = (0, 100, 255)
+ORANGE = (255, 69, 0)
+GREEN1 = (0, 128, 0)
+BLUE2 = (50, 205, 50)
 BLACK = (0, 0, 0)
 
 
@@ -85,9 +85,9 @@ class AIGame:
     def _update_ui(self):
         self.display.fill(BLACK)
         for pt in self.snake:
-            pygame.draw.rect(self.display, BLUE1, pygame.Rect(pt.x, pt.y, BLOCK_SIZE, BLOCK_SIZE))
+            pygame.draw.rect(self.display, GREEN1, pygame.Rect(pt.x, pt.y, BLOCK_SIZE, BLOCK_SIZE))
             pygame.draw.rect(self.display, BLUE2, pygame.Rect(pt.x + 4, pt.y + 4, 12, 12))
-        pygame.draw.rect(self.display, RED, pygame.Rect(self.food.x, self.food.y, BLOCK_SIZE, BLOCK_SIZE))
+        pygame.draw.rect(self.display, ORANGE, pygame.Rect(self.food.x, self.food.y, BLOCK_SIZE, BLOCK_SIZE))
         text = font.render("Score: " + str(self.score), True, WHITE)
         self.display.blit(text, [0, 0])
         pygame.display.flip()
